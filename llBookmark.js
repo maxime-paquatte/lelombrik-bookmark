1e5a5ef0f10e4044011d33b0c1b18c5e154e55ff
nevaram <nevaram@gmail.com>
2014-10-06 15:56:33 +0200

    Fix button position for unregistered users + Use chrome.storage

--- llBookmark.js
+++ llBookmark.js
@@ -4,26 +4,35 @@
     var host = location.protocol + '//' + location.host;
 
     if (location.hash == '#bookmark') {
-        var savedId = parseInt(localStorage['bookmark']);
-        if (!isNaN(savedId)) {
-            location.href = host + '/' + savedId;
-        } else alert('Aucun marque page enregistré');
+        chrome.storage.sync.get('bookmark', function (r) {
+            var bookmark = r.bookmark;
+            if (!isNaN(bookmark)) {
+                location.href = host + '/' + bookmark;
+            } else alert('Aucun marque page enregistré');
+        });
+        
     }
     var fileId = parseInt(document.location.pathname.substr(1));
     if (!isNaN(fileId)) {
-        var nextItem = document.getElementsByClassName('report_file')[0];
-        var container = nextItem.parentElement;
-
+     
         var button = document.createElement("a");
         button.className = "bookmark";
         button.setAttribute("href", host + "/1#bookmark");
-        button.setAttribute("title", "Cliquez pour enregistrer le marque page / Glissez ce lien dans vos favoris pour revenir à ce fichier");
+        button.setAttribute("title", "Cliquez pour enregistrer le marque page / Glissez ce lien dans vos favoris pour revenir au dernier fichier marqué");
         button.setAttribute("style", 'background: url("' + chrome.extension.getURL("clock.png") + '") no-repeat;');
 
-        container.insertBefore(button, nextItem);
+        var container = document.getElementsByClassName('view_file')[0];
+        container.appendChild(button);
 
+        //If user is auth
+        var reportButton = document.getElementsByClassName('report_file')[0];
+        if (reportButton) button.style.right = "95px";
+         
+        
         button.onclick = function () {
-            localStorage.setItem('bookmark', fileId);
+            chrome.storage.sync.set({ 'bookmark': fileId }, function () {
+                button.className = "bookmark success";
+            });
             return false;
         };
     }
